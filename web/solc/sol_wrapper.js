780e7d7acfa05b90c5329b8e4247e0ebf1214749
Denis <1duglas1@gmail.com>
2020-03-02 16:46:18 +0200

    Impemented tabs

--- web/solc/sol_wrapper.js
+++ web/solc/sol_wrapper.js
@@ -1,24 +1,26 @@
-var solidity_compile_wrap = cwrap('solidity_compile', 'string', ['string', 'number']);
-window.ast_gen = function(code){
+var solidity_compile_wrap = cwrap("solidity_compile", "string", [
+  "string",
+  "number"
+]);
+window.ast_gen = function(code) {
   input = {
-    language: 'Solidity',
+    language: "Solidity",
     sources: {
-        'test.sol': {
-            content: code
-        }
+      "test.sol": {
+        content: code
+      }
     },
     settings: {
-        
-        outputSelection: {
-            '*': {
-                '*': [ '*' ],
-                '' : ['ast']
-            }
+      outputSelection: {
+        "*": {
+          "*": ["*"],
+          "": ["ast"]
         }
+      }
     }
-  }
+  };
   var output = JSON.parse(solidity_compile_wrap(JSON.stringify(input)));
-  
+
   var error, is_ok, res, _i, _len, _ref;
 
   is_ok = true;
@@ -26,7 +28,7 @@ window.ast_gen = function(code){
   _ref = output.errors || [];
   for (_i = 0, _len = _ref.length; _i < _len; _i++) {
     error = _ref[_i];
-    if (error.type === 'Warning') {
+    if (error.type === "Warning") {
       console.log("WARNING", error);
       continue;
     }
@@ -38,11 +40,11 @@ window.ast_gen = function(code){
     throw Error("solc compiler error");
   }
 
-  res = output.sources['test.sol'].ast;
+  res = output.sources["test.sol"].ast;
 
   if (!res) {
     throw Error("!res");
   }
 
   return res;
-}
+};
